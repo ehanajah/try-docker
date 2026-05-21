f8963a414cdd31af75134108a6c71f620f965c26
Raihan Najah <ehanajah@users.noreply.github.com>
2026-05-21 15:42:05 +0700

    refactor: add CI workflow for automated testing and deployment

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,16 @@
+from quart import current_app
+
 from app.main import create_app
 
 app = create_app()
+
+@app.route("/health")
+async def health():
+    await current_app.db.command("ping")
+
+    return {
+        "status": "ok"
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
